33182ad2a6e44e7ec9cba883e387c03d0cb115d8
sherrydv <sherryxdev@gmail.com>
2025-11-11 01:35:05 +0100

    proton-ci: added proton-be CI build

---
+++ patches/proton-be/apply.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu
+
+patch_cmd() {
+    echo "Applying:: $(basename "$1")"
+    patch -Np1 -i "$1"
+}
+
+here="$(dirname "$(realpath "$0")")"
+    
+if [ -d "$here/proton" ] && [ "$(ls -A "$here/proton")" ]; then
+    mapfile -t ppatches < <(find "$here/proton" -type f -name "*.patch" | sort)
+    for patch in "${ppatches[@]}"; do
+        patch_cmd "$patch"
+    done
+fi
+
+cd wine || exit 1
+
+if [ -d "$here/wine" ] && [ "$(ls -A "$here/wine")" ]; then
+    mapfile -t patches < <(find "$here/wine" -type f -name "*.patch" | sort)
+    for patch in "${patches[@]}"; do
+        patch_cmd "$patch"
+    done
+fi
+
+cd ..
